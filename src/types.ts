8b939de779d8b339787b7839eda5cb1da4f988f1
mirumi <mail@mirumi.me>
2022-06-11 11:32:20 +0900

    Delete src directory

--- src/types.ts
+++
@@ -1,4 +0,0 @@
-export type Fetch = typeof globalThis.fetch
-export type RequestInfo = globalThis.RequestInfo
-export type RequestInit = globalThis.RequestInit
-export type Response = globalThis.Response
